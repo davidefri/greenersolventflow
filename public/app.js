7bb76b3ce927d326a9e7aa9c2833cb6c2d8c6acf
Davide Frigatti <davide.frigatti@gmail.com>
2025-11-24 13:51:51 +0100

    Aggiunta definitiva della cartella public per Cloudflare Pages

---
+++ public/app.js
@@ -0,0 +1,5 @@
+// public/app.js
+
+// !!! SOSTITUISCI CON IL TUO URL PUBBLICO !!!
+const API_URL = 'https://api-worker.davide-frigatti.workers.dev'; 
+// Il resto del codice in app.js può rimanere lo stesso
